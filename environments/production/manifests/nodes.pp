d459a052ac08c04dc450fc763f3843fa6a6adc26
root <root@10.1.104.36 puppet.kocloudy.com puppet>
2015-03-11 13:40:14 -0400

    Add production environment and enable automatic provisioning of new nodes.

---
+++ environments/production/manifests/nodes.pp
@@ -0,0 +1,38 @@
+node default {
+  include puppet
+  service { 'puppet':
+    enable => true,
+    ensure => running,
+  }
+  file { '/tmp/puppet-ip-10-1-165-117.txt':
+    content => "Hello world",
+  }
+}
+node 'kopuppet01' {
+  include puppet
+  file { '/tmp/hello.txt':
+    content => "Hello world.\nHow;s Trix?\n"
+  }
+  file { '/home/ec2-user/bonehead':
+    ensure => present,
+    content => "Don't be one.\n",
+  }
+}
+node 'kopuppet02' {
+  include puppet
+  file { '/tmp/puppet.hi':
+    content => 'Hi there.\n',
+  }
+}
+node 'kopuppet03' {
+  include puppet
+  file { '/tmp/puppet.hi':
+    content => "Hi there, number 3.\n",
+  }
+}
+node 'puppet' {
+  file { '/tmp/puppet.hi':
+    content => 'Hi there.\n',
+  }
+}
+	
